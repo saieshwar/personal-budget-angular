78d70aa18ed65a9232f2ecadbf05f21c172d90c1
saieshwar <bobbilisaieshwarreddy@gmail.com>
2020-10-03 04:22:55 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,9 +4,43 @@ const port = 3000;
 const fs = require('fs');
 
 app.use('/', express.static('public'));
-let rawdata = fs.readFileSync('myBudget.json');
-let student = JSON.parse(rawdata);
 
+//Reading from JSON file
+let rawdata = fs.readFileSync('myBudget.json');
+let my_budget = JSON.parse(rawdata);
+
+
+
+app.get('/hello', (req,res) => { 
+    res.send('Hello World');
+});
+
+// app.get('/budget', (req, res) =>{
+//     res.json(budget); 
+// });
+
+
+//sending response as a json
+app.get('/budget', (req, res) =>{
+    res.json(my_budget); 
+});
+
+
+app.listen(port, () => {
+    console.log('Example app listening at http://localhost:'+port);
+});
+
+
+
+
+
+
+
+
+
+
+
+//references
 // const budget = {
 //     myBudget: [
 //     {
@@ -23,21 +57,3 @@ let student = JSON.parse(rawdata);
 //     },
 //   ]
 // };
-
-app.get('/hello', (req,res) => { 
-    res.send('Hello World');
-});
-
-// app.get('/budget', (req, res) =>{
-//     res.json(budget); 
-// });
-
-app.get('/budget', (req, res) =>{
-    res.json(student); 
-});
-
-
-
-app.listen(port, () => {
-    console.log('Example app listening at http://localhost:'+port);
-});
